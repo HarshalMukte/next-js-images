acf4260bf29c484d41c6b4bf6fb76e7ed6e4baa3
Harshal <harshalmukte@gmail.com>
2024-01-27 09:37:30 +0530

    update-1

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,29 @@
 import styles from "./page.module.css";
 import Image from "next/image";
-import staticImage from "../../public/image.jpg"
+import ImageSrc from "../../public/image.jpg"
 
 const Home = () => {
   return (
     <main className={styles.wrapper}>
+      {/* Static image using the normal <img> tag */}
       <div className={styles.staticImage}>
         <p className={styles.heading}>Static Image(img)</p>
-        <img src={staticImage.src} alt="image" />
+        <div className={styles.imageDiv}>
+          {/* <img src={ImageSrc.src} alt="image" /> */}
+        </div>
+      </div>
+      {/* Dynamic image using the next <Image> tag */}
+      <div className={styles.staticImage}>
+        <p className={styles.heading}>Static Image(img)</p>
+        <div className={styles.imageDiv}>
+          <Image 
+          src={ImageSrc}
+          alt="image"
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          placeholder="blur"
+          />
+        </div>
       </div>
     </main>
   )
