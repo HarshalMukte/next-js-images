72b73f2cd974df59d99a3f71ac080fc13aa18dd4
Harshal <harshalmukte@gmail.com>
2024-01-27 10:19:06 +0530

    final commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,25 +3,27 @@ import Image from "next/image";
 import ImageSrc from "../../public/image.jpg"
 
 const Home = () => {
+  const imageURL = "https://images.unsplash.com/photo-1604500693431-647f9e76dafc?q=80&w=3465&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
   return (
     <main className={styles.wrapper}>
       {/* Static image using the normal <img> tag */}
       <div className={styles.staticImage}>
         <p className={styles.heading}>Static Image(img)</p>
         <div className={styles.imageDiv}>
-          {/* <img src={ImageSrc.src} alt="image" /> */}
+          <img src={ImageSrc.src} alt="image" />
         </div>
       </div>
-      {/* Dynamic image using the next <Image> tag */}
+      {/* Static image using the next <Image> tag */}
       <div className={styles.staticImage}>
-        <p className={styles.heading}>Static Image(img)</p>
+        <p className={styles.heading}>Static Image(next/Image)</p>
         <div className={styles.imageDiv}>
           <Image 
           src={ImageSrc}
           alt="image"
           fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           placeholder="blur"
+          sizes="25vw"
           />
         </div>
       </div>
